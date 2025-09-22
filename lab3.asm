367ed41a5bbf7714c6b887d929cd10207b656745
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-09-22 16:49:43 +0800

    add comments to lab3.asm

--- lab3.asm
+++ lab3.asm
@@ -8,10 +8,10 @@ main EQU start@0
 .code
 Main PROC
     mov ecx, 9; length of Result is 9, hence we need to loop 9 times
-    mov esi, 0;
+    mov esi, 0; initialize the index to 0
     mov al, 9; initialize al
 
-L1: mov Result[esi], al;
+L1: mov Result[esi], al; copy the value from al to memory
     add al, 9; accumulating the value 9, 18, 27, ...
     inc esi ; incrementing the index
     loop L1 ; looping
