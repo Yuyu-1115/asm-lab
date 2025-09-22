88e954e10599e54cd2f2fdd2fc8d2b3be80d0ad8
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-09-22 16:03:32 +0800

    lab3

---
+++ lab3.asm
@@ -0,0 +1,21 @@
+TITLE lab3
+INCLUDE Irvine32.inc
+
+main EQU start@0
+
+.data
+    Result byte 9 DUP(?)
+.code
+Main PROC
+    mov ecx, 9; length of Result is 9, hence we need to loop 9 times
+    mov esi, 0;
+    mov al, 9; initialize al
+
+L1: mov byte[esi], al;
+    add al, 9; accumulating the value 9, 18, 27, ...
+    inc esi ; incrementing the index
+    loop L1 ; looping
+
+    exit
+main ENDP
+END main
