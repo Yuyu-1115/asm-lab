daea50b181a39e3af54e0b959411cabbc3439959
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-09-29 23:08:48 +0800

    Add comments for Lab4

--- lab4.asm
+++ lab4.asm
@@ -11,24 +11,24 @@ main PROC
     mov ebx,9999h;
     mov ecx,9999h;
     mov edx,9999h;
-    mov esi,OFFSET myID;
-    mov ecx,size_ID
-    call Convert
+    mov esi,OFFSET myID; Get the memory address of myID
+    mov ecx,size_ID; Set the loop counter to 9
+    call Convert; call the procedure with USES macro
 
-    mov esi,OFFSET myID2
-    mov ecx,size_ID
-    call Convert2
+    mov esi,OFFSET myID2; update the pointer
+    mov ecx,size_ID; redundant
+    call Convert2; call the procedure withour USES macro
     
 exit
 main ENDP
 
 Convert PROC USES eax ebx ecx edx	;
 L1:
-	mov al,[esi];
-    sub al,'0';
-    add al,'A';
-    mov [esi],al;
-    inc esi;
+	mov al,[esi]; Copy the data from memory to register
+    sub al,'0';; Get the numerical value of character
+    add al,'A';; Map 0-A, 1-B, and so on...
+    mov [esi],al; Send the value back to registers
+    inc esi; Increment the index or the pointer
     loop L1
     ret
 Convert ENDP
