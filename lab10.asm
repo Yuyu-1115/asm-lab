97e79f948b00517f503db1991f754b47bc67d519
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-11-10 16:06:41 +0800

    lab10

---
+++ lab10.asm
@@ -0,0 +1,79 @@
+INCLUDE Irvine32.inc
+
+ 
+
+Str_copyN PROTO, 
+    source:PTR BYTE,   ; source string address
+    target:PTR BYTE,   ; target string address
+    maxChars:DWORD      ; maximum number of characters to copy
+
+
+main          EQU start@0
+
+.data
+
+string_1 BYTE "113502017",0    ; student ID1
+
+string_2 BYTE "113502023",0    ; student ID2
+
+.code
+
+main PROC 
+
+    INVOKE Str_copyN,        ; copy string_1 to string_2
+      OFFSET string_1,        
+      OFFSET string_2,     
+      (SIZEOF string_1) - 1  ; save space for null byte
+
+ 
+
+; Display the destination string
+
+    mov  edx, OFFSET string_2
+
+    call WriteString
+
+    call Crlf
+
+    call WaitMsg
+
+    exit
+
+main ENDP
+
+ 
+
+Str_copyN PROC USES eax ecx esi edi,
+    source:PTR BYTE,     ; source string
+    target:PTR BYTE,     ; target string
+    maxChars:DWORD       ; max chars to copy
+
+; Copy a string from source to target, limiting the number
+
+; of characters to copy by macChars.
+
+; The value in maxChars does not include the null byte.
+
+ 
+
+    mov ecx, LENGTHOF source       ; set counter for REP
+
+    mov esi, source       ; assign source
+
+    mov edi, target + LENGTHOF target       ; assign target
+
+    cld                 ; clear direction flag (direction = forward)
+
+    rep movsb           ; copy the string
+
+    mov [esi], 0            ; set null byte to register
+
+    movsb               ; append null byte
+
+    ret 12            ; clear data in stack
+
+Str_copyN ENDP
+
+ 
+
+END main
